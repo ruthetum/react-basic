250875f1490d072e02d9347a5c3ec5a4b729d11b
Heedong Kim <ruthetum@gmail.com>
2020-08-14 16:36:46 +0900

    네이버 영화 API 받기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import axios from "axios";
 
 class App extends React.Component{
   state = {
-    isLoading: true
+    isLoading: true,
+    movies: [],
+    value: ""
   };
   
+  getMoives = async() => {
+    const ID_KEY = 'id';
+    const SECRET_KEY = 'key';
+    const search = this.state.value;
+
+    const {
+      data: {
+        data: {movies}
+      }
+    } = await axios.get('https://openapi.naver.com/v1/search/movie.json' ,{
+      params:{ query: search, display: 20 },
+      headers: { 'X-Naver-Client-Id': ID_KEY, 'X-Naver-Client-Secret': SECRET_KEY }
+    });
+    this.setState({ movies, isLoading:false });
+    console.log(movies);
+  };
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false});
-    }, 6000);
+    this.getMoives();
   }
 
   render() {
