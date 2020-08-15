d1fbceb7c40b1f5379491431d69b6834bd44229e
Heedong Kim <ruthetum@gmail.com>
2020-08-16 03:21:28 +0900

    Updates

---
+++ precache-manifest.9b3988503b2820a22061959d52a725e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7a6f851b794a4274015baed618dd4ab",
+    "url": "/react-basic/index.html"
+  },
+  {
+    "revision": "3e12e99b4705a17018a5",
+    "url": "/react-basic/static/css/main.b812e9d5.chunk.css"
+  },
+  {
+    "revision": "3c68698531b925235846",
+    "url": "/react-basic/static/js/2.ab63a2cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-basic/static/js/2.ab63a2cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e12e99b4705a17018a5",
+    "url": "/react-basic/static/js/main.52100759.chunk.js"
+  },
+  {
+    "revision": "9489638870b1d22104c4",
+    "url": "/react-basic/static/js/runtime-main.ead89368.js"
+  }
+]);
