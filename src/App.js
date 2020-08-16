207407563bce60827188ab5fb78b6a891fc5a605
Heedong Kim <ruthetum@gmail.com>
2020-08-16 16:15:36 +0900

    react router

--- src/App.js
+++ src/App.js
@@ -1,92 +1,13 @@
 import React from 'react';
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
 
-class App extends React.Component{
-  state = {
-    isLoading: true,
-    movies: [],
-    value: ""
-  };
-  
-  getMovies = async() => {
-    const ID_KEY = 'sNxTbgnt_IcCpjBvC1Cd';
-    const SECRET_KEY = 'r7C6l7qZRA';
-    const search = this.state.value;
-
-    try {
-      if (search === "") {
-        this.setState({ movies: [], isLoading: false });
-      } else {
-        const {
-          data: { items }
-        } = await axios.get(
-            '/api/v1/search/movie.json' ,{
-            params:{
-              query: search,
-              display: 10
-            },
-            headers: {
-              'X-Naver-Client-Id': ID_KEY,
-              'X-Naver-Client-Secret': SECRET_KEY
-            }
-        });
-  
-        this.setState({ movies: items, isLoading: false });
-      } 
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  componentDidMount() {
-    this.getMovies();
-  };
-
-  handleChange = (e) => {
-    this.setState({ value: e.target.value });
-  };
-  
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.getMovies();
-  };
-
-
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-        <section className="container">
-            {isLoading ? (
-              <div className="loader">
-                <span className="loader_text">Loading...</span>
-              </div>
-            ) : (<form onSubmit={this.handleSubmit}>
-              <div className="input_div">
-                <h1>영화 검색</h1>
-                <input
-                  className="input_search"
-                  type="text"
-                  value={this.state.value}
-                  onChange={this.handleChange}
-                  placeholder="영화를 검색해 보세요."/>
-              </div>
-              <div className="movies">
-                {movies.map(movie => (
-                  <Movie
-                    id={movie.link}
-                    title={movie.title}
-                    poster={movie.image}
-                    actors={movie.actor}
-                    year={movie.pubDate}
-                  />
-                ))}
-              </div>
-            </form>)}
-        </section>
-    );
-  }
+function App() {
+  return <HashRouter>
+    <Route path="/" exact={true} component={Home} />
+    <Route path="/about" component={About} />
+  </HashRouter>;
 }
 
 export default App;
